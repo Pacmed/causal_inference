d2efec26fcb2d123462d75c6e5ed68877b3c2a66
adam.izdebski <adam.izdebski@pacmed.nl>
2021-03-23 17:49:06 +0100

    experiments added

--- causal_inference/model/ols.py
+++ causal_inference/model/ols.py
@@ -1,57 +1,88 @@
 """
-This module implements outcome regression.
+This module implements simple outcome regression (1-OLS or S-OLS).
 """
 
 import numpy as np
+import statsmodels.api as sm
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from statsmodels.tools.eval_measures import rmse
 from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics import euclidean_distances
 
-class TemplateEstimator(BaseEstimator):
-    """ A template estimator to be used as a reference implementation.
-    For more information regarding how to build your own estimator, read more
-    in the :ref:`User Guide <user_guide>`.
+from causal_inference.model.utils import calculate_rmse, calculate_r2
 
-    Parameters
-    ----------
-    demo_param : str, default='demo_param'
-        A parameter used for demonstation of how to pass and store paramters.
-
+class OLS(BaseEstimator):
     """
-    def __init__(self, demo_param='demo_param'):
-        self.demo_param = demo_param
+    A simple outcome regression estimator.
+    """
+    def __init__(self, random_state=None):
+        self.random_state = random_state
 
-    def fit(self, X, y):
-        """A reference implementation of a fitting function.
+    def fit(self, X, y, t=None):
+        """
+        Fits the simple outcome regression to data.
+
         Parameters
         ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        X : array-like, shape (n_samples, n_features)
             The training input samples.
         y : array-like, shape (n_samples,) or (n_samples, n_outputs)
-            The target values (class labels in classification, real numbers in
-            regression).
+            The target values.
+        t : array-like, shape (n_samples,) or (n_samples, n_outputs)
+            The training input treatment values.
+
         Returns
         -------
         self : object
             Returns self.
         """
-        X, y = check_X_y(X, y, accept_sparse=True)
+
+
+        X = np.hstack((t, X))
+        X, y = check_X_y(X, y)
+        print(y.shape, y.shape, X.shape)
+        self.model_ = sm.OLS(y, sm.add_constant(X)).fit()
+
+        self.rmse_ = calculate_rmse(y, self.model_.predict(sm.add_constant(X)))
+        self.r2_ = calculate_r2(y, self.model_.predict(sm.add_constant(X)))
         self.is_fitted_ = True
-        # `fit` should always return `self`
+
         return self
 
-    def predict(self, X):
-        """ A reference implementation of a predicting function.
+    def predict(self, X, t=None):
+        """
+        Makes predictions with the simple outcome regression.
+
         Parameters
         ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
-            The training input samples.
+        X : array-like, shape (n_samples, n_features)
+            The test input samples.
+
+        t : array-like, shape (n_samples,) or (n_samples, n_outputs)
+            The training input treatment values.
+
+
         Returns
         -------
-        y : ndarray, shape (n_samples,)
-            Returns an array of ones.
+        y : ndarray, shape (n_samples, 2)
+            Returns an array of predicted outcomes.
         """
+
         X = check_array(X, accept_sparse=True)
         check_is_fitted(self, 'is_fitted_')
-        return np.ones(X.shape[0], dtype=np.int64)
+
+        X_treated = np.hstack((np.zeros((X.shape[0], 1), dtype=np.int64), X))
+        X_control = np.hstack((np.ones((X.shape[0], 1), dtype=np.int64), X))
+        print(X.shape, X_control.shape, X_treated.shape)
+        m_1 = self.model_.predict(sm.add_constant(X_treated))
+        m_0 = self.model_.predict(sm.add_constant(X_control))
+
+
+        return np.Series(m_1, m_0)
+
+    def predict_ate(self):
+        return self.model_.params[1]
+
+    def score(self, data, targets, treatment):
+        return calculate_r2(targets, self.predict(data)[treatment])
