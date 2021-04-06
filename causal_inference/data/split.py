88a40e3064a3b5af40c9dc5a2fa21f248bf67a77
adam.izdebski <adam.izdebski@pacmed.nl>
2021-04-06 10:23:17 +0200

    split working

--- causal_inference/data/split.py
+++ causal_inference/data/split.py
@@ -1,37 +1,20 @@
-""" This module performs a train/test split on the relevant np.ndarrays.
+""" This module performs a train/test split of the relevant np.ndarrays.
 """
 
-import pandas as pd
 import numpy as np
 
-import sys, os
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from causalinference import CausalModel
-
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import roc_auc_score
-from importlib import reload
-
-from scipy.stats import wasserstein_distance
-from scipy import stats
+from typing import Optional
+from math import floor
 
 
-def train_test_split(y: np.ndarray,
-                     t: np.ndarray,
-                     X: np.ndarray,
-                     random_state: bool=None):
+def train_test_split(y:np.ndarray,
+                     t:np.ndarray,
+                     X:np.ndarray,
+                     frac:Optional[float]):
 
-    # TO DO: how random state works?
+    idx_treated = np.random.randint(y[t].shape[0], size=floor(len(y[t])*frac))
+    idx_control = np.random.randint(y[~t].shape[0], size=floor(len(y[~t]) * frac))
 
-    treated = y[t].sample(frac=size, random_state=random_state).index.to_list()
-    control = y[~t].sample(frac=size, random_state=random_state).index.to_list()
-
-    # TO DO: Check compatibility with np
-    idx = y.index.isin(treated + control)
+    idx = np.concatenate((idx_treated, idx_control))
 
     return y[idx], t[idx], X[idx], y[~idx], t[~idx], X[~idx]
-
