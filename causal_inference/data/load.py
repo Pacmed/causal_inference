80f7c1e5396888a8d3a214fccc58cfc4291b673e
adam.izdebski <adam.izdebski@pacmed.nl>
2021-04-06 11:04:12 +0200

    Minor fixes.

--- causal_inference/data/load.py
+++ causal_inference/data/load.py
@@ -10,9 +10,9 @@ from typing import Optional
 def prepare_csv_data(df:pd.DataFrame,
                      outcome_name:str,
                      threshold:float=1):
-    """Prepares the csv data to be converted into np.ndarrays.
+    """Transforms raw data into data ready to be converted into np.ndarrays.
 
-    Prepares the data by removing redundant outcomes, and dtypes conversion.
+    transforms the data by removing redundant outcomes, and dtypes conversion.
 
     Parameters
     ----------
@@ -30,7 +30,7 @@ def prepare_csv_data(df:pd.DataFrame,
         DataFrame with covariates, treatment indicator and outcome, ready to be converted into np.ndarray.
     """
 
-    # Delete additional outcomes
+    # Delete additional pre-existing outcomes
     outcomes_to_delete = df.filter(regex='outcome').columns.to_list()
     outcomes_to_delete.remove(outcome_name)
     df.drop(columns=outcomes_to_delete, inplace=True)
@@ -43,8 +43,10 @@ def prepare_csv_data(df:pd.DataFrame,
     df = df.dropna(thresh=thresh, axis=1)
 
     # Convert categorical variable into dummy/indicator variables.
+    columns_not_to_drop = df.filter(regex='False').columns.to_list()
     df = pd.get_dummies(df)
     columns_to_drop = df.filter(regex='False').columns.to_list()
+    columns_to_drop = list(set(columns_to_drop) - set(columns_not_to_drop))
     columns_to_drop = ['gender_M'] + columns_to_drop if 'gender_M' in df.columns else columns_to_drop
     df.drop(columns=columns_to_drop, inplace=True)
 
@@ -93,8 +95,7 @@ def csv_data_to_np(df:pd.DataFrame,
 
 def csv_data_to_covariates_names(df: pd.DataFrame,
                                  outcome_name: str,
-                                 treatment_name: str,
-                                 covariates_name: str = None):
+                                 treatment_name: str):
     """Returns names of the covariates.
 
     Parameters
@@ -105,8 +106,6 @@ def csv_data_to_covariates_names(df: pd.DataFrame,
         Name of the outcome column.
     treatment_name : str
         Name of the treatment indicator column.
-    covariates_name : Optional[str]
-        Name of the covariates columns. If None, then all columns are included.
 
     Returns
     -------
