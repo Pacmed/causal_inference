a43b2602ffec185dbccc6a0a5a80528fb1c50bb3
adam.izdebski <adam.izdebski@pacmed.nl>
2021-04-12 13:48:05 +0200

    Documentation added

--- causal_inference/make_data/make_data.py
+++ causal_inference/make_data/make_data.py
@@ -1,36 +1,28 @@
 """ This module extracts raw data from the covid data warehouse.
 """
-import os
 
-import pandas as pd
-
-from datetime import datetime
 from data_warehouse_utils.dataloader import DataLoader
 
 from causal_inference.make_data.make_proning_sessions import make_proning_sessions, COLUMNS_POSITION
 
 
 class UseCaseLoader(DataLoader):
-    """
-    A class used to extract raw data from the covid data warehouse and save it into a directory.
-
-    Attributes
-    ----------
-    path : str
-        Path to a directory to save the raw data into.
-    datetime : str
-        Datetime of the initialization of the UseCaseLoader class. Used in he file name of the extracted data.
-        Format: %Y-%m-%d-%H:%M:%S'
-    Methods
-    -------
-
-
+    """Loads data for the purpose of the causal experiment.
     """
     def __init__(self):
         super().__init__()
 
     def get_position_measurements(self, path):
-        """ Extracts position measurements.
+        """Saves and extracts raw data of position measurements from the data warehouse.
+
+        Parameters
+        ----------
+        path : str
+            Path to save the extracted data.
+
+        Returns
+        -------
+        z : None
         """
 
         self.get_range_measurements(parameters=['position'], columns=COLUMNS_POSITION).to_csv(path, index=False)
@@ -38,9 +30,27 @@ class UseCaseLoader(DataLoader):
         return None
 
     @staticmethod
-    def get_unique_sessions(load_path, save_path):
+    def make_unique_sessions(load_path, save_path, n_of_batches=None):
+        """Saves and transforms raw data of position measurements saved by 'get_position_measurements' method.
 
-        make_proning_sessions(load_path).to_csv(path_or_buf=save_path, index=False)
+        In the transformed data, each row is a unique supine/prone session with a 'start_timestamp', 'end_timestamp'
+        and 'duration_hours'.
+
+        Parameters
+        ----------
+        load_path : str
+            A path to the raw data.
+        save_path : str
+            A path to save the transformed data.
+        n_of_batches : Optional[int]
+            Number of batches to be included. Use only for testing purposes.
+
+        Returns
+        -------
+        z : None
+        """
+
+        make_proning_sessions(load_path, n_of_batches).to_csv(path_or_buf=save_path, index=False)
 
         return None
 
