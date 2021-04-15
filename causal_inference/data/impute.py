5c3d0548ee6b9529cea86281d881ad471977bf6a
adam.izdebski <adam.izdebski@pacmed.nl>
2021-04-15 16:45:29 +0200

    add details to impute docstring

--- causal_inference/data/impute.py
+++ causal_inference/data/impute.py
@@ -7,12 +7,12 @@ from sklearn.impute import SimpleImputer
 
 
 def impute(X:np.ndarray):
-    """Imputes missing values with the mean of the column.
+    """Imputes missing values with the mean of the column. All bool and int values should not be missing!
 
     Parameters
     ----------
     X : np.ndarray
-        Covariates with missing values.
+        Covariates with missing values for numerical columns.
 
     Returns
     -------
@@ -20,7 +20,6 @@ def impute(X:np.ndarray):
         Covariates without missing values.
     """
 
-    # TO DO: Ensure that the missing values for bool and int are fixed before and only floats are imputed.
     imp = SimpleImputer(missing_values=np.nan, strategy='mean')
     imp.fit(X)
     X = imp.transform(X)
