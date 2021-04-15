4173b048c5e66f68174e7f5a70477f9cb3b78593
adam.izdebski <adam.izdebski@pacmed.nl>
2021-04-15 16:37:01 +0200

    fix train_test_split

--- causal_inference/data/split.py
+++ causal_inference/data/split.py
@@ -10,7 +10,7 @@ from math import floor
 def train_test_split(y:np.ndarray,
                      t:np.ndarray,
                      X:np.ndarray,
-                     frac:Optional[float]=0.8):
+                     train_size:Optional[float]=0.8):
     """Split the data into a train and test set.
 
     Train and test set are stratified on the treatment indicator 't'.
@@ -40,9 +40,17 @@ def train_test_split(y:np.ndarray,
         Test covariates.
     """
 
-    idx_treated = np.random.randint(y[t].shape[0], size=floor(len(y[t])*frac))
-    idx_control = np.random.randint(y[~t].shape[0], size=floor(len(y[~t]) * frac))
+    assert np.array_equal(t, t.astype(bool))
 
-    idx = np.concatenate((idx_treated, idx_control))
+    split_treated = floor(len(y[t]) * train_size)
+    split_control = floor(len(y[~t]) * train_size)
 
-    return y[idx], t[idx], X[idx], y[~idx], t[~idx], X[~idx]
+    indices_treated = np.random.permutation(y[t].shape[0])
+    indices_control = np.random.permutation(y[~t].shape[0])
+
+    training_idx = np.concatenate((indices_treated[:split_treated], indices_control[:split_control]))
+    test_idx = np.concatenate((indices_treated[split_treated:], indices_control[split_control:]))
+
+    training_idx, test_idx = np.random.shuffle(training_idx), np.random.shuffle(test_idx)
+
+    return y[training_idx], t[training_idx], X[training_idx, :], y[test_idx], t[test_idx], X[test_idx, :]
